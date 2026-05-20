61fff6ac10fb541005aad1ab88480635e751fd3f
Mattéo Diseurs <diseursmat@gmail.com>
2026-05-20 20:14:20 +0200

    Add project statistics to Accueil view: initialize `ProjectManager` in `AccueilController`, update template to display `nbrProject`

--- Src/Controller/AccueilController.php
+++ Src/Controller/AccueilController.php
@@ -3,17 +3,20 @@
 namespace DISEUMAT\Controller;
 
 use DISEUMAT\Model\Service\Manager\JobManager;
+use DISEUMAT\Model\Service\Manager\ProjectManager;
 use DISEUMAT\Model\Service\Manager\TechManager;
 
 class AccueilController extends BaseController
 {
     private TechManager $TM;
     private JobManager $JB;
+    private ProjectManager $PM;
 
     public function __construct(){
         parent::__construct();
         $this->TM = new TechManager();
         $this->JB = new JobManager();
+        $this->PM = new ProjectManager();
     }
 
     public function formAccueil() : void{
@@ -21,9 +24,11 @@ class AccueilController extends BaseController
 
         $nbrTech = count($this->TM->list());
         $nbrJobs = count($this->JB->list());
+        $nbrProject = count($this->PM->list());
 
         echo $this->TemplateEngine->render('/Accueil/Accueil.twig',
             ['nbrTech' => $nbrTech,
-                'nbrJobs' => $nbrJobs]);
+                'nbrJobs' => $nbrJobs,
+                'nbrProject' => $nbrProject]);
     }
 }

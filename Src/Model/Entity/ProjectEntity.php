3307a74b78b5a5f9829b54fce4d032489e9c5dd6
Mattéo Diseurs <diseursmat@gmail.com>
2026-05-20 19:08:00 +0200

    Expand `ProjectEntity` class: add properties, getters, setters, and constructor for handling project data comprehensively.

--- Src/Model/Entity/ProjectEntity.php
+++ Src/Model/Entity/ProjectEntity.php
@@ -4,5 +4,90 @@ namespace DISEUMAT\Model\Entity;
 
 class ProjectEntity
 {
+    private int $Pk;
+    private string $Ident;
+    private string $Descr;
+    private string $Dstart;
+    private string $DClotEst;
+    private int $budget;
+    private array $Jobs;
+    public function __construct(){
+        $this->Pk = 0;
+        $this->Ident = "";
+        $this->Descr = "";
+        $this->Dstart = "";
+        $this->DClotEst = "";
+        $this->budget = 0;
+        $this->Jobs = array();
+    }
+    public function getPk(): int
+    {
+        return $this->Pk;
+    }
+
+    public function setPk(int $Pk): void
+    {
+        $this->Pk = $Pk;
+    }
+
+    public function getIdent(): string
+    {
+        return $this->Ident;
+    }
+
+    public function setIdent(string $Ident): void
+    {
+        $this->Ident = $Ident;
+    }
+
+    public function getDescr(): string
+    {
+        return $this->Descr;
+    }
+
+    public function setDescr(string $Descr): void
+    {
+        $this->Descr = $Descr;
+    }
+
+    public function getJobs(): array
+    {
+        return $this->Jobs;
+    }
+
+    public function setJobs(array $Jobs): void
+    {
+        $this->Jobs = $Jobs;
+    }
+
+    public function getBudget(): int
+    {
+        return $this->budget;
+    }
+
+    public function setBudget(int $budget): void
+    {
+        $this->budget = $budget;
+    }
+
+    public function getDClotEst(): string
+    {
+        return $this->DClotEst;
+    }
+
+    public function setDClotEst(string $DClotEst): void
+    {
+        $this->DClotEst = $DClotEst;
+    }
+
+    public function getDstart(): string
+    {
+        return $this->Dstart;
+    }
+
+    public function setDstart(string $Dstart): void
+    {
+        $this->Dstart = $Dstart;
+    }
 
 }
